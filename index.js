2b7993f38e3091c33a789c98f8ddc8b41dc78e0f
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-13 08:44:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById("main");
+
+mainElement.parentNode.removeChild(mainElement);
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = "Marvyn is the champion";
